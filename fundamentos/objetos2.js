029f9c5a9f663b6247be0380108754f9583bb2e6
Dahan <Dahan@AJUADMNTB021.ifs.edu.br>
2019-11-08 02:18:05 -0300

    Aula 3.37

---
+++ fundamentos/objetos2.js
@@ -0,0 +1,10 @@
+console.log(typeof Object)
+console.log(typeof new Object())
+
+const Func = function() {}
+console.log(typeof Func)
+console.log(typeof new Func)
+
+class Klass {}
+console.log(typeof Klass)
+console.log(typeof new Klass)
