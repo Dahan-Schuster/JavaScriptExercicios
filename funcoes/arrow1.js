085c374f4f13e500f13e3307a8f8d88550be6f2f
Dahan <Dahan@AJUADMNTB021.ifs.edu.br>
2019-11-09 20:33:27 -0300

    Aula 5.8

---
+++ funcoes/arrow1.js
@@ -0,0 +1,15 @@
+let dobro = function(a) {
+    return 2 * a
+}
+
+dobro = (a) => {
+    return 2 * a
+}
+
+dobro = a => 2 * a // return implícito
+
+console.log(dobro(Math.PI))
+
+ola = () => 'Olá'
+ola = _ => 'Olá' // possui um parâmetro, mas está sendo ignorado
+console.log(ola())
