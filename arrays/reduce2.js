3363a95a81f06a20d069117a7e017ab081718f4a
Dahan Schuster <dahan@moobitech.com.br>
2020-01-21 17:06:40 -0300

    Resolve os desafios em reduce2.js

--- arrays/reduce2.js
+++ arrays/reduce2.js
@@ -1,11 +1,20 @@
 const alunos = [
-    {nome: 'João', nota: 7.3, bolsista: false},
-    {nome: 'Maria', nota: 9.2, bolsista: true},
-    {nome: 'Pedro', nota: 9.8, bolsista: false},
-    {nome: 'Ana', nota: 8.7, bolsista: true}
+    { nome: 'João', nota: 7.3, bolsista: false },
+    { nome: 'Maria', nota: 9.2, bolsista: true },
+    { nome: 'Pedro', nota: 9.8, bolsista: false },
+    { nome: 'Ana', nota: 8.7, bolsista: false }
 ]
 
-// Desafio 1: todos os alunos são bolsistas?
+const alunosSaoBolsitas = alunos.map(aluno => aluno.bolsista)
 
+// Desafio 1: todos os alunos são bolsistas?
+const conferirSeTodosSaoBolsistas = (outrosSaoBolsistas, esseEhBolsista) => outrosSaoBolsistas && esseEhBolsista
+const todosSaoBolsistas = alunosSaoBolsitas.reduce(conferirSeTodosSaoBolsistas, true)
+
+console.log('Resposta desafio 1 ->', todosSaoBolsistas ? 'Sim' : 'Não')
 
 // Desafio 2: Algum aluno é bolsista?
+const conferirSeAlgumEhBolsista = (outrosSaoBolsistas, esseEhBolsista) => outrosSaoBolsistas || esseEhBolsista
+const algumEhBolsista = alunosSaoBolsitas.reduce(conferirSeAlgumEhBolsista, false)
+
+console.log('Resposta desafio 2 ->', algumEhBolsista ? 'Sim' : 'Não')
